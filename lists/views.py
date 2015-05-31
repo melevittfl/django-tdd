1db68431d245129ad1a927bf6653d87b5ad0726a
Mark Levitt <mark@marklevitt.co.uk>
2015-05-31 23:08:47 +0100

    Save items to the DB. Bottom of page 66

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from lists.models import Item
 from django.http import HttpResponse
 
 
 # Create your views here.
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', '')})
+        'new_item_text': item.text})
