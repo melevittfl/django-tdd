dbca46a2af451768d4017f28f7b901b2759f134d
Mark Levitt <mark@marklevitt.co.uk>
2015-06-13 18:29:37 +0100

    Redirect after POST and show all items in template

--- functional_test.py
+++ functional_test.py
@@ -1,11 +1,13 @@
+from django.http.request import HttpRequest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from lists.models import Item
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': item.text})
+    items = Item.objects.all()
+
+    return render(request, 'home.html', {'items': items})
